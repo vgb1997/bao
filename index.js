8d2a737ec891ef72100146a58228943e1b7df3b5
nguyenthininh <45448845+nguyenthininh@users.noreply.github.com>
2019-05-20 16:22:55 +0700

    them vai dong

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.get('/abc-def', function (req, resp) {
     console.send("Hello abc def");
 });
 
+app.post('/demo', function (req, resp) {
+    resp.send("heloo"+ req.query.name );
+});
 
 app.post('/demo', function (req, resp) {
     resp.send("heloo"+ req.query.name );
